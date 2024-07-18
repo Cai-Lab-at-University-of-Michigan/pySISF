8c44955a4f555670acef54c026f59c8a0ea7ccac
Logan Walker <loganaw@umich.edu>
2024-07-18 13:33:19 -0400

    Update vidlib.py

--- src/pySISF/vidlib.py
+++ src/pySISF/vidlib.py
@@ -18,9 +18,7 @@ def encode_stack(input_stack, method=EncoderType.X264, debug=False):
     w = input_stack.shape[1]
     h = input_stack.shape[2]
 
-    nodes = ffmpeg.input(
-        "pipe:", format="rawvideo", pix_fmt="gray", s=f"{h}x{w}", framerate="24/1"
-    )
+    nodes = ffmpeg.input("pipe:", format="rawvideo", pix_fmt="gray", s=f"{h}x{w}", framerate="24/1")
 
     match method:
         case EncoderType.X264:
@@ -80,21 +78,15 @@ def encode_stack(input_stack, method=EncoderType.X264, debug=False):
     out, err = process.communicate(input=to_encoder)
 
     if not len(out):
-        raise ValueError(
-            "No output receieved from ffmpeg. Is your chunk size sufficient?"
-        )
+        raise ValueError("No output receieved from ffmpeg. Is your chunk size sufficient?")
 
     return out
 
 
 def decode_stack(input_blob, dims=(128, 128), method="libx264", debug=False):
-    nodes = ffmpeg.input("pipe:", framerate="24/1").output(
-        "pipe:", format="rawvideo", pix_fmt="gray", framerate="24/1"
-    )
+    nodes = ffmpeg.input("pipe:", framerate="24/1").output("pipe:", format="rawvideo", pix_fmt="gray", framerate="24/1")
 
-    process = nodes.run_async(
-        cmd=fmpeg_exe, pipe_stdout=True, pipe_stdin=True, pipe_stderr=(not debug)
-    )
+    process = nodes.run_async(cmd=fmpeg_exe, pipe_stdout=True, pipe_stdin=True, pipe_stderr=(not debug))
 
     process.stdin.write(input_blob)
     process.stdin.close()
